89fac080a0a9f9280313dd8f371d41720a6e36c3
Rulo <tecoatl_mx@hotmail.com>
2021-02-25 23:23:47 -0600

    Mi primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloWorld } from './primercomponente/primer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorld
   ],
   imports: [
     BrowserModule
---
+++ src/app/primercomponente/primer.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'hello-world',
+    //template: '<h1>Hola mundo 1</h1>'
+    templateUrl: './primer.component.html',
+    styles: ['h1 {background: *fff; color: #000;}']
+    //styleUrls: ['./primer.component.css']
+})
+export class HelloWorld {
+
+}
